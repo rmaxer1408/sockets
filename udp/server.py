a7e3d1205dfd43abff0a8ea9f0f073e7bf3271ea
rmaxer1408 <rmnosipov59@gmail.com>
2022-12-08 12:38:59 +0600

    feat: add a loop in server

--- udp/server.py
+++ udp/server.py
@@ -1,13 +1,18 @@
 import socket
 
-
 HOST = '127.0.0.1'
 PORT = 8888
-buffer = 2**10
+buffer = 2 ** 10
 message_s = 'Message'
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_sock.bind((HOST, PORT))
-result = server_sock.recv(buffer)
-print(message_s, result.decode('utf-8'))
-server_sock.close()
+
+while True:
+    try:
+        result = server_sock.recv(buffer)
+    except KeyboardInterrupt:
+        server_sock.close()
+        break
+    else:
+        print(message_s, result.decode('utf-8'))
